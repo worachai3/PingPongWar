666cad6e24739cffb23c7a1e35d494b4e3165c4a
worachai3 <worachai3@live.com>
2017-09-29 11:50:29 +0700

    added players

--- ping_pong_war.py
+++ ping_pong_war.py
@@ -9,6 +9,18 @@ class PingPongWarWindow(arcade.Window):
 
         arcade.set_background_color(arcade.color.BLACK)
 
+        self.player1_sprite = arcade.Sprite('images/Player1.png')
+        self.player1_sprite.set_position(20, 300)
+
+        self.player2_sprite = arcade.Sprite('images/Player2.png')
+        self.player2_sprite.set_position(580, 300)
+
+    def on_draw(self):
+        arcade.start_render()
+
+        self.player1_sprite.draw()
+        self.player2_sprite.draw()
+
 def main():
     window = PingPongWarWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
     arcade.set_window(window)
